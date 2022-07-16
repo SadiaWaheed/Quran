944f73a0c3ec090583774b3258bd9666e3a34378
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-17 00:10:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#homePage').click(function () {
     var data = $(this).load('1.html .ayah');      
-    var json = JSON.stringify(data)
+    var json = JSON.stringify({html:data})
     console.log(json)
   });
 });
