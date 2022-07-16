adc1a581d1a5c6f3c11321eb6fec64e42ead2761
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-16 22:49:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $('#homePage').load(function () {
+ 
     const ayahs = []
       for(let i = 1; i< 115; i++){
         ayahs[i] = $(this).load(i+'.html .ayah');
       }    
       console.log(ayahs)
-   });
+   
 });
