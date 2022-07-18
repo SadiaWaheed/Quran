110ce33efc1999781b405facb4dc78edd8ade30b
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-18 17:33:33 +0300

    limit to 100 for test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
   $('#homePage').click(function () {
     $(this).load('1.html .ayah')
-    for(let i = 2; i<=114 ; i++){
+    for(let i = 100; i<=114 ; i++){
       $('body').append($('<div class="surah">').load(i+'.html .ayah'))
     }
   });
