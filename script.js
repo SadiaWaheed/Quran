b9cf736f607df751688b06cd92b987caeb74191b
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-19 17:51:28 +0300

    -

--- script.js
+++ script.js
@@ -10,32 +10,37 @@ $(document).ready(function () {
   $('#result').click(function () {
     var surah = []
     jsonData = null;
-    s=112
-    $('body').load(s+'.html .ayah')
-  
-    $('.surah').each(function(){
-      var  data = []
-      a=1
-      
-      $('.ayah').each(function(){
-        var item = $(this)
-        data.push({
-          ayah_no: a,
-          indo_text: $('span', item).text()
-        });  
-        a++;
-        console.log("a="+a)
-        console.log(data)
+
+    for(let i = 112; i <=114; i++){
+      $('body').load(s+'.html .ayah')
+
+      $('.surah').each(function(){
+        var  data = []
+        a=1
+        
+        $('.ayah').each(function(){
+          var item = $(this)
+          data.push({
+            ayah_no: a,
+            indo_text: $('span', item).text()
+          });  
+          a++;
+          console.log("a="+a)
+          console.log(data)
+        });
+        console.log("final a = "+a)
+        surah.push({
+          surah_no:i,
+          text:data
+        });      
+        s++; 
+        console.log("i= "+i)   
+       console.log(surah)
       });
-      console.log("final a = "+a)
-      surah.push({
-        surah_no:s,
-        text:data
-      });      
-      s++; 
-      console.log("s= "+s)   
-     console.log(surah)
-    });
+    }
+    
+  
+    
     document.body.innerHTML=""
     jsonData = JSON.stringify(surah)
     document.body.innerHTML = jsonData
