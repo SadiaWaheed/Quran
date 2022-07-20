9459843f9e1df08c6aa098825bf9a0b2a0759f12
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-20 20:16:21 +0300

    -

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
  
  $('#homePage').click(function () {
   $(this).load('112.html .112-ayah')
-
+for(let i =113; i<=114; i++){
     $('body').append($('<div class="surah">').load(i+'.html .'+i+'-ayah'))
     
   }
