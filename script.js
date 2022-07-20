7b60295ef0429effc92e1dc1245d0d3f99181de3
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-20 18:55:48 +0300

    -

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
  $('#homePage').click(function () {
   $(this).load('112.html .ayah')
-  document.body.getElementsByClassName("ayah").className = "ayah-112"
+  
   for(let i = 113; i<=114 ; i++){
 
     $('body').append($('<div class="surah">').load(i+'.html .ayah'))
-    document.body.getElementsByClassName("ayah").className = "ayah-"+i
+   
   }
 });
 
