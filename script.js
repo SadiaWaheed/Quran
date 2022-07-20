2bdf41a5c090283aa9b73826e34259dba9dc174e
SadiaWaheed <51211519+SadiaWaheed@users.noreply.github.com>
2022-07-20 18:46:51 +0300

    -

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 
-$(document).ready(function () {
-  $('#result').click(function () {
-    var surah = []
-    jsonData = null;
-    for(let i = 1; i <=5; i++){     
-      $('body').load(i+'.html .ayah')
-        var  data = []
-        a=1        
-        $('.ayah').each(function(){
-          var item = $(this)
-          data.push({
-            ayah_no: a,
-            indo_text: $('span', item).text()
-          });  
-          a++;
-          console.log("a="+a)
-          console.log(data)
-        });
-        console.log("final a = "+a)
-        surah.push({
-          surah_no:i,
-          text:data
-        }); 
-        console.log("i= "+i)   
-       console.log(surah)     
-    }    
-    jsonData = JSON.stringify(surah)
-    document.body.innerHTML =  $('body').append(jsonData)
-    });
+ $('#homePage').click(function () {
+  $(this).load('112.html .ayah')
+  for(let i = 113; i<=114 ; i++){
+
+    $('body').append($('<div class="surah">').load(i+'.html .ayah'))
+    document.body.getElementsByClassName("ayah").className = "ayah-"+i
+  }
 });
+
+$('#result').click(function () {
+  var surah = []
+  jsonData = null;
+  s=112
+
+  $('.surah').each(function(){
+    var  data = []
+    a=1
+    
+    $('.ayah-'+s).each(function(){
+      var item = $(this)
+      data.push({
+        ayah_no: a,
+        indo_text: $('span', item).text()
+      });  
+      a++;
+      console.log("a="+a)
+      console.log(data)
+    });
+    console.log("final a = "+a)
+    surah.push({
+      surah_no:s,
+      text:data
+    });      
+    s++; 
+    console.log("s= "+s)   
+   console.log(surah)
+  });
+  document.body.innerHTML=""
+  jsonData = JSON.stringify(surah)
+  document.body.innerHTML = jsonData
+
+  });
+
